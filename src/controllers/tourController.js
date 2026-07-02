4e8761a163967a24bdcef22bf8880438f999e367
hansh_x <hans.devme100@gmail.com>
2026-07-02 14:05:10 +0530

    Implementing  filtering

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -1,13 +1,83 @@
+const Tour = require("../models/tourModel");
+
 /*==>  
     desc: Fetch all tours from DB
     route: [GET]:   /tours 
     access: public 
  <== */
-const getAlltours = (req, res, next) => {
-  res.json({
-    status: "success",
-    message: "All tours fetched successfully",
-  });
+const getAlltours = async (req, res, next) => {
+  try {
+    //==> Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ["limit", "page", "sort", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    //==> Converting query string to mongoDB filter query
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|lte|gt|lt)\b/g,
+      (match) => `$${match}`,
+    );
+
+    console.log(JSON.parse(queryString));
+    const query = Tour.find(JSON.parse(queryString));
+
+    const tours = await query;
+    res.status(200).json({
+      status: "success",
+      result: tours.length,
+      data: { tours: tours },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "fail",
+      message: "Error! Cannot fetch tours",
+    });
+  }
+};
+
+/*==>  
+    desc: Fetch single tour by Id
+    route: [GET]:   /tours/:tourId 
+    access: private 
+ <== */
+const getTour = async (req, res, next) => {
+  const tourId = req.params.tourId;
+  try {
+    const tour = await Tour.findById(tourId);
+    res.status(200).json({
+      status: "success",
+      data: { tour },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "fail",
+      message: "Error! Cannot fetch tour",
+    });
+  }
+};
+
+/*==>  
+    desc: Create a new tour
+    route: [POST]:   /tours 
+    access: private 
+ <== */
+const createTour = async (req, res, next) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(200).json({
+      status: "success",
+      data: { tour: newTour },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "fail",
+      message: "Error! Cannot create a new tour",
+    });
+  }
 };
 
 /*==>  
@@ -15,11 +85,24 @@ const getAlltours = (req, res, next) => {
     route: [PATCH]:   /tours/:ID 
     access: private 
  <== */
-const updateTour = (req, res, next) => {
-  res.json({
-    status: "success",
-    message: "Tour updated successfully",
-  });
+const updateTour = async (req, res, next) => {
+  const tourId = req.params.tourId;
+  if (!tourId) throw new Error("Invalid Id");
+  try {
+    const tour = await Tour.findByIdAndUpdate(tourId, req.body, {
+      returnDocument: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: tour,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error,
+    });
+  }
 };
 
 /*==>  
@@ -28,24 +111,20 @@ const updateTour = (req, res, next) => {
     access: private 
  <== */
 
-const deleteTour = (req, res, next) => {
-  res.json({
-    status: "success",
-    data: null,
-  });
-};
-
-/*==>  
-    desc: Fetch single tour by Id
-    route: [GET]:   /tours/:tourId 
-    access: private 
- <== */
-const getTour = (req, res, next) => {
+const deleteTour = async (req, res, next) => {
   const tourId = req.params.tourId;
-  res.status(200).json({
-    status: "success",
-    data: null,
-  });
+  try {
+    await Tour.findByIdAndDelete(tourId);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "success",
+      message: error,
+    });
+  }
 };
 
-module.exports = { getAlltours, updateTour, getTour };
+module.exports = { getAlltours, updateTour, getTour, createTour, deleteTour };
--- src/routes/tourRoutes.js
+++ src/routes/tourRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getAlltours, getTour } = require("../controllers/tourController");
+const {
+  getAlltours,
+  getTour,
+  createTour,
+  updateTour,
+  deleteTour,
+} = require("../controllers/tourController");
 
 const router = express.Router();
 
-router.route("/").get(getAlltours);
-router.route("/:tourId").get(getTour);
+router.route("/").get(getAlltours).post(createTour);
+router.route("/:tourId").get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
